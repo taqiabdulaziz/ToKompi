b51fba135b9543666fe1bb917bebe9edbe1e83ab
taqiabdulaziz <svx.annexiv@gmail.com>
2019-01-10 19:28:15 +0700

    add require mongoose app.js

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # ToKompi
+API
+
+```
+POST - /login
+
+POST - /auth
+
+```
+
+```
+
+POST - /cart/:id_user
+
+DELETE - /cart/:id_user/:id_item
+
+GET - /cart/:id_user
+
+
+```
+```
+UPDATE - /user/:id_user
+
+DELETE - /user/:id_user - A
+
+```
+
+
+```
+POST - /transaction/:id_user
+
+GET - /transaction/:id_user
+```
+
+```
+
+GET - /items
+
+POST - /items/ - A
+
+UPDATE - /items/?id_item - A
+
+DELETE - /items/?id_item - A
+
+```
+
--- server/app.js
+++ server/app.js
@@ -3,16 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require(`mongoose`)
+var cors = requier(`cors`)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+mongoose.connect('mongodb://localhost/tokompi', { useNewUrlParser: true });
 var app = express();
-
+var db = mongoose.connection;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log(`we're connected`);
 
+});
+
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,12 +32,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
